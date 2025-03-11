d319f42fc6f9b816367672db5273990a80d24240
baodnc1 <miosazanami2@gmail.com>
2025-03-11 16:02:49 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,17 @@
 // () => {}
 // component = html + css + js
+
+import './style.css';
+
   const MyComponent = () => {
     return (
-      <div> abcdefgh </div>
+        <>
+            
+            <div> abcdefgh </div>
+            <div className='child'>child</div>
+        </>
     );
   }
 
 
   export default MyComponent;
-  
