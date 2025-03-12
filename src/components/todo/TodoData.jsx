caa98cfa74428db7a561796d12fdd5bb4a4d5cc9
baodnc1 <miosazanami2@gmail.com>
2025-03-12 21:09:45 +0700

    video 34

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,9 @@
 
-const TodoData = () => {
+const TodoData = (props) => {
+    console.log(">>> check props: ", props)
     return (
         <div className='todo-data'>
+            <div>My name is {props.name}</div>
             <div>Learning React</div>
             <div>Watching youtube</div>
         </div>
