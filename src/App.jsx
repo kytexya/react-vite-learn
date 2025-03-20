d7a970005e27ad51ffa86167fd88adfea2b6358e
baodnc1 <miosazanami2@gmail.com>
2025-03-20 15:46:07 +0700

    video 42

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,12 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter(item => item.id !== id)
+    setTodoList(newTodo);
+  
+  }
+
   
   
   return (
@@ -40,6 +46,7 @@ const App = () => {
        {todoList.length > 0 ?
       <TodoData
         todoList={todoList}
+        deleteTodo={deleteTodo}
       />     
       :  
         <div className='todo-image'>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,14 @@
 //props la mot bien object {}
 const TodoData = (props) => {
 
-const { todoList} = props;
+const { todoList, deleteTodo} = props;
+
+
+const handleClick = (id) => {
+    deleteTodo(id)
+
+}
+
 
     return (
         <div className='todo-data'>
@@ -10,7 +17,9 @@ const { todoList} = props;
                     return (
                     <div className={'todo-item'} key={item.id}>
                             <div> {item.name}</div>                      
-                        <button>Delete</button>
+                        <button
+                            onClick={() => handleClick(item.id)}
+                            style={{cursor:"pointer"}}>Delete</button>
                     </div>)
                 })}
 
