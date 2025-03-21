45ca6fd747b90da8ba97bc88199677f87dd07598
baodnc1 <miosazanami2@gmail.com>
2025-03-21 20:06:30 +0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import{
 } from "react-router-dom";
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
-import ProductPage from './pages/product.jsx';
+import BookPage from './pages/book.jsx';
 import UserPage from './pages/user.jsx';
 import './styles/global.css'
 import TodoApp from './components/todo/TodoApp.jsx';
@@ -26,8 +26,8 @@ const router = createBrowserRouter([
         element: <UserPage />
       },
       {
-        path: "/products",
-        element: <ProductPage />
+        path: "/books",
+        element: <BookPage />
       }
 
     ]
