93de83922e625b12e850137143acbc9fd5eef578
baodnc1 <miosazanami2@gmail.com>
2025-03-28 16:39:38 +0700

    video 71

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,7 +1,7 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import Password from "antd/es/input/Password";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -9,16 +9,22 @@ const UserForm = () => {
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email, 
-            password: password,
-            phone: phone
+    const handleClickBtn = async () => {
+       const res= await createUserAPI(fullName, email, password, phone);
+       
+       if( res.data ) {
+            notification.success({
+                message: "Create user successfully",
+                description: "User created successfully",               
+            })
+       }else {
+            notification.error({
+                message: "Error create user",
+                description: JSON.stringify(res.message)
+            })
         }
-        axios.post(URL_BACKEND, data)
-        console.log(">>> chech state:",{ fullName, email, password, phoneNumber });
+            
+        console.log(">>> chech res :" , res.data);
     }
     return (
         <div className="user-form" style={{margin: "20px 0"}}>
